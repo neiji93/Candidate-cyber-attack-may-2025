75053e077446b08047fb951897bbbf76a8e8114a
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2025-05-12 09:54:18 +0200

    Update GeneralizedDecompositionForCoroutinesSolver.h

--- GeneralizedDecompositionForCoroutinesSolver.h
+++ GeneralizedDecompositionForCoroutinesSolver.h
@@ -1,1 +1,5 @@
-
+//integral h mult v.
+//can be used to optimize the decomposition
+//if we use it with my computer vision proiject one 
+/)repository 
+//then here we will decompose prime numbers digits.
